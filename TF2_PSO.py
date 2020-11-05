3833ebd1cea9045b0cd6b6f3d8fceaad3a9da0b0
markliou <mark.liu@coretronic.com>
2020-11-06 01:03:54 +0800

    fix the reference

--- TF2_PSO.py
+++ TF2_PSO.py
@@ -6,27 +6,26 @@ import tensorflow_datasets as tfds
 class PSO():
     def __init__(self, 
                  TF2_model, 
-                 pbest_wma = .8,
-                 update_w = .8,
+                 update_w = .2,
                  update_c1 = 2,
                  update_c2 = 2,
-                 population_size = 50):
+                 population_size = 100):
         self.nnmodel = TF2_model
         self.population_size = population_size
         self.population = self._createPopulation()
-        self.pbest_wma = pbest_wma
         self.update_w = update_w
         self.update_c1 = update_c1
         self.update_c2 = update_c2
         self.pbest = {'fitness':tf.zeros([population_size]), 
                       'weights':tf.zeros(self.population['weights'].shape)}
+        self.force_evaluate = True
     pass 
 
     def _createPopulation(self):
         self.population = {}
         # creating the weights
         weights = self._flattenWeightsTFKeras()
-        self.population['weights'] = tf.stack([tf.random.normal(weights.shape, stddev=1.) for i in range(self.population_size)], axis=0)
+        self.population['weights'] = tf.stack([tf.random.normal(weights.shape, stddev=.5) for i in range(self.population_size)], axis=0)
         self.population['velocity'] = tf.stack([tf.random.normal(weights.shape, stddev=.05) for i in range(self.population_size)], axis=0)
         # creating the nn body for parallel computing
         # self.population['graphs'] = [tf.keras.models.clone_model(self.nnmodel) for i in range(self.population_size)]
@@ -43,7 +42,7 @@ class PSO():
         for model_tesnsor in model.trainable_weights:
             element_shape = model_tesnsor.shape.as_list()
             element_number = np.ones(element_shape).sum().astype('int')
-            model_tesnsor.assign(tf.reshape(flated_weights[access_index:access_index+element_number], element_shape))
+            model_tesnsor.assign(tf.reshape(flated_weights[access_index:access_index + element_number], element_shape))
             access_index += element_number
         pass
         return model
@@ -58,32 +57,43 @@ class PSO():
         return tf.concat(fitness_rec, axis=0)
     pass 
 
-    def minimize(self):
+    def minimize(self, fitness_function):
         # get fitnesses of each individual
-        fitness_rec = self.update_fitness()
+        fitness_rec = self.update_fitness(fitness_function)
+        # print(fitness_rec)
 
         # take change to jump out of the outlier
         # 有可能當下抽樣計算出的loss異常的高。如果抽到這樣的outliner可能會把族群trap住。
         # 所以pbest的紀錄用moving average稍微處理
-        self.pbest['fitness'] = fitness_rec * (1 - self.pbest_wma) + self.pbest['fitness'] * self.pbest_wma
+        if self.force_evaluate:
+            self.force_evaluate = False
+            self.pbest['fitness'] = tf.identity(fitness_rec)
+            self.pbest['weights'] = tf.identity(self.population['weights'])
+        else :
+            self.pbest['fitness'] = tf.identity(fitness_rec + self.pbest['fitness'])/2 
+        pass 
         
         # update pbest memory
         rec_cmp = tf.cast(tf.math.less(self.pbest['fitness'], fitness_rec), dtype=tf.float32)
-        self.pbest['fitness'] = self.pbest['fitness'] * rec_cmp + fitness_rec * (-1 * (rec_cmp - 1)) # update the pbest fitness
+        self.pbest['fitness'] = tf.identity(self.pbest['fitness'] * rec_cmp + fitness_rec * (-1 * rec_cmp + 1)) # update the pbest fitness
         rec_cmp = tf.reshape(rec_cmp, [-1, 1])
-        self.pbest['weights'] = self.pbest['weights'] * rec_cmp + self.population['weights'] * (-1 * (rec_cmp - 1)) # update the pbest weights
+        # print(rec_cmp)
+        self.pbest['weights'] = tf.identity(self.pbest['weights'] * rec_cmp + self.population['weights'] * (-1 * rec_cmp + 1)) # update the pbest weights
         
         # create gbest tensors
-        gbest = tf.stack([self.pbest['weights'][tf.argmax(fitness_rec)] for i in range(self.population_size)], axis=1)
+        gbest = tf.identity(self.population['weights'][tf.argmin(fitness_rec)])
 
         # update population 
         # vid+1 = w∙vid+c1∙rand()∙(pid-xid)+c2∙Rand()∙(pgd-xid) 
         # xid+1 = xid+vid
-        self.population['velocity'] = self.update_w * self.population['velocity'] + \
-                                      self.update_c1 * tf.random.uniform(self.population['velocity'].shape) * (self.pbest['weights'] - self.self.population['weights']) + \
-                                      self.update_c2 * tf.random.uniform(self.population['velocity'].shape) * (gbest - self.self.population['weights'])
+        self.population['velocity'] = tf.identity(
+                                      self.update_w * tf.identity(self.population['velocity']) +\
+                                      self.update_c1 * tf.math.abs(tf.random.normal(self.population['velocity'].shape, stddev=.05, dtype=tf.float32)) * tf.identity(self.pbest['weights'] - self.population['weights']) +\
+                                      self.update_c2 * tf.math.abs(tf.random.normal(self.population['velocity'].shape, stddev=.05, dtype=tf.float32)) * tf.identity(gbest - self.population['weights'])
+                                      )
+        self.population['weights'] = tf.identity(self.population['velocity'] + self.population['weights'])
 
-        self.population['weights'] += self.population['velocity']
+        return self.pbest['fitness'][tf.argmin(self.pbest['fitness'])]
     pass 
 
     def getTopModel(self):
@@ -127,6 +137,7 @@ def main():
     ###################
     def loss():
         #print(cnn(data_fetcher['image']))
+
         return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = np.reshape(data_fetcher['label'], [-1, 1]), logits = cnn(data_fetcher['image'])))
     pass   
     ###################
@@ -134,6 +145,14 @@ def main():
     print(loss())
 
     opt = PSO(cnn)
+    print(opt.minimize(loss))
+    
+    # optimization
+    while(1):
+    # for steps in range(20):
+        # data_fetcher = mnist_tr_iter.next()
+        print(opt.minimize(loss))
+    pass
 
 
 pass 
