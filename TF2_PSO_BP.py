b3413cf11861c4ce36f5568b017d77a375b64eaf
root <yifanliou@gmail.com>
2020-11-07 14:53:06 +0000

    set the interial

--- TF2_PSO_BP.py
+++ TF2_PSO_BP.py
@@ -113,7 +113,7 @@ class PSO():
                                       )
         self.population['weights'] = tf.identity(self.population['velocity'] + self.population['weights'])
 
-        self.update_w *= self.interial
+        self.update_w *= self.update_interial
         
         return self.pbest['fitness'][tf.argmin(self.pbest['fitness'])]
     pass 
